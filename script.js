044474108d21494ab7623a8a27e4eba143cce05b
cbedroid <cbedroid1614@gmail.com>
2021-09-27 02:22:17 -0400

    Seperated html and css from script, renamed script

--- script.js
+++ script.js
@@ -10,183 +10,19 @@
 
 /* Show FreeCode Camp  Responsive HTML Error*/
 
+// Github Repo File Paths
+const REPO_BASE_URL="https://github.com/cbedroid/FreeCodeCamp-FailedTestSolver"
+const REPO_GHP_URL="https://cbedroid.github.io/FreeCodeCamp-FailedTestSolver"
+const STYLESHEET = `${REPO_GHP_URL}/styles.css`;
+const HTML_FILE = `${REPO_GHP_URL}/content.html`;
+
 /* Injects css style */
 (function injectStyle() {
-  var style = document.createElement("style");
-  style.type = "text/css";
-  style.innerHTML = `
-    #freecodecamp_mod{ 
-        position: fixed;
-        top: 2px;
-        right: 5px;
-        z-index: 50000;
-        width: 320px;
-        height: 300px;
-        margin: 10px;
-        color: #000;
-        background: #d0d0d5;
-        box-shadow: 2px 2px 8px rgba(0,0,0,.4);
-        border: 1px solid rgba(0,0,0,.4);
-        border-radius: 8px;
-        transition: .3s ease-in-out;
-        overflow: auto;
-     }
-      #freecodecamp_mod.hidden{
-        dispay: grid;
-        place-items: center;
-        width: 32px;
-        height: 32px;
-        font-size: 1.15em;
-        background: transparent;
-        border-radius: 2px;      
-        overflow: hidden;
-      } 
-
-     #freecodecamp_mod.hidden *:not(.content-wrapper,.btn-wrapper, .btn){
-      display: none;
-    } 
-
-    #freecodecamp_mod #content-wrapper{ 
-      position: relative;
-    }
-
-    #freecodecamp_mod .btn-wrapper{ 
-      position: absolute;
-      top: 2px;
-      left: 2px;
-    }
-    #freecodecamp_mod.hidden .btn-wrapper{ 
-      position: absolute;
-      top: -2px;
-      left:0;
-      display: block;
-      height: 100%;
-      margin: auto;
-    }
-
-    #freecodecamp_mod .btn{
-        display: none;
-        opacity: 1;
-        width: fit-content;
-        max-height: 20px;
-        padding: 3px;
-        font-size: 1.25em;
-        font-weight: 700;
-        color: #fff;
-        background: transparent;
-        border: none;
-        outline: none;
-        text-shadow: 2px 2px 2px rgba(0,0,0,.5);
-        transition: .2s ease-in-out;
-    }
-    #freecodecamp_mod .btn.active{
-      display: block;
-    }
-    #freecodecamp_mod #close_btn::before {
-        content: '\\2613';
-    }
-
-    #freecodecamp_mod .btn-wrapper #open_btn::before{
-      content: '\\2630';
-    }
-
-    #freecodecamp_mod .title{ 
-      font-weight: 700;
-      text-align: center;
-      padding: 5px;
-      color: #fff;
-      background: #0a0a23;
-      border-bottom: 2px solid #000;
-    }
-    #freecodecamp_mod .runner-container{
-      padding: 3px;
-    }
-    #freecodecamp_mod .mod_selector{
-      display: block;
-      width: 100%;
-    }
-
-    #freecodecamp_mod #test_runner_btn{
-      font-weight: 700;
-      margin-left: 3px;
-      color: #fff;
-      background:#0a0a3a;
-      border-radius: 4px;
-      box-shadow: 2px 2px 8px rgba(0,0,0,.4);
-      transform: translate(0, -10px);
-    } 
-    #freecodecamp_mod #test_runner_btn:hover{
-      background:#0a3a82;
-    }
-    #freecodecamp_mod #failure_list{
-     display: block;
-     min-height: 113px;
-     height: 100%;
-     padding: 5px 0px 4px;
-    }
-    #freecodecamp_mod .result-item{
-      list-style: none;
-      color: #000;
-      font-weight: 300;
-      line-height: 1em;
-      padding: 12px 8px;
-      border-bottom: 1px solid #000;
-    }
-    #freecodecamp_mod .result-item:nth-child(even){
-      background: #fff;
-    }
-    #freecodecamp_mod #suite_footer{
-      width:100%;
-      text-align:center;
-      background: #e9eabc;
-      padding: 4px;
-     }
-
-    #freecodecamp_mod #suite_footer small {
-      color:333;
-      font-size: .75em;
-      font-weight:400;
-      margin: 2px 6px;
-      padding: 6px;
-    }
-    #freecodecamp_mod #suite_footer small a{
-      transition: .2s ease-in;
-      font-weight: 700;
-      color: #0a0a23;
-    }
-    #freecodecamp_mod #suite_footer small a:hover{
-      color: #00f;
-    }
-
-  #freecodecamp_mod .loader-wrapper {
-    display: none;
-  }
-  #freecodecamp_mod .loader-wrapper.active {
-
-      position: absolute;
-      top: 0;
-      left: 0;
-      display: grid;
-      place-items:center;
-      width: 100%;
-      height: 100%;
-  }
-  #freecodecamp_mod .loader {
-    border: 8px solid #f3f3f3; /* Light grey */
-    border-top: 8px solid #3498db; /* Blue */
-    border-radius: 50%;
-    display: block;
-    width: 100px;
-    height: 100px;
-    animation: spin 1s linear infinite;
-  }
-
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }`;
-
-  document.getElementsByTagName("head")[0].appendChild(style);
+  var link = document.createElement("link");
+  link.href = STYLESHEET;
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
 })();
 
 /* Inject FreeCode Script if not loaded */
@@ -215,8 +51,20 @@
     .insertBefore(fcc_script, last_script[last_script.length || 0]);
 })();
 
+function fetchFile(url) {
+  var request = new XMLHttpRequest();
+  request.open( "GET", url true);
+  request.send(null);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      return request.responseText;
+    }
+  };
+}
+
 function runMain() {
   buildMarkup();
+  document.querySelector("#repo_link").href=REPO_BASE_URL
   let root = document.getElementById("fcc_test_suite_wrapper").shadowRoot;
   let suit_ui = root.querySelector(".fcc_test_ui");
   let fcc_run_button = suit_ui.querySelector(
@@ -257,44 +105,20 @@ function runMain() {
    *  Build main container for test results.
    */
   function buildMarkup() {
-    const markup = `
-       <div class="content-wrapper">
-         <div class="loader-wrapper">
-          <div class="loader"></div>
-         </div>
-        <div class="btn-wrapper">
-         <button id="close_btn" class="btn active" type="button"></button>
-         <button id="open_btn" class="btn"></button>
-        </div>
-        <h2 class="title">
-        <img src="https://cdn.rawgit.com/Deftwun/e3756a8b518cbb354425/raw/6584db8babd6cbc4ecb35ed36f0d184a506b979e/free-code-camp-logo.svg" width="25" height="25" style="background:#fff;">
-        FCC Test Mod
-        </h2>
-        <div class="runner-container">
-         <br>
-         <button id="test_runner_btn">Run Test</button>
-        </div>
-        <ul id="failure_list" class="result-list">     
-        </ul>
-       </div>
-
-       <div id="suite_footer">
-        <small>created by: 
-         <a href="https://github.com/cbedroid/" target="_blank">cbedroid</a>
-        </small>
-        <small>
-         <a href="https://github.com/cbedroid/freecodecamp_suite_mod/" target="_blank">
-          <svg height="20" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="20" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
-           <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-          </svg>
-          Github Repo
-         </a>
-        </small>
-      </div>
-     `;
     const main_element = document.createElement("div");
+    const markup = fetchFile(HTML_FILE);
     main_element.id = "freecodecamp_mod";
-    main_element.innerHTML = markup;
+    if (markup){
+      main_element.innerHTML = markup;
+    }else{
+      main_element.innerHTML = `
+        <div class="error" style="text-align:center;">
+          <h3 style="font-weight:bold;color:red">FCC_ERROR: HTML content was not found</h3>
+          <small>If this issue persists, submit an issue <a href="${REPO_BASE_URL}/issues/">here<a/><small>  
+        </div>
+        `;
+    }
+    
     document.getElementsByTagName("body")[0].appendChild(main_element);
   }
 
@@ -303,7 +127,6 @@ function runMain() {
     const suite_selector = suit_ui.querySelector("#test-suite-selector");
     // Mod Suite Selector
     const mod_fcc_selector = suite_selector.cloneNode(true);
-    console.log({ mod_fcc_selector });
     mod_fcc_selector.classList.add("mod_selector");
 
     /* Send mod_fcc_selector change event to FCC Suite Selector and Vice-Versa
@@ -397,7 +220,6 @@ function runMain() {
    * set FCC visible state from localStorage
    */
   function setStorageVisibilty(state = true) {
-    console.log("setting Visible state to", state);
     localStorage.FCCMod_visible = state;
   }
 }
